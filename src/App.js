5357c5aa45b567ee45f3a0bfa0d64e17d46c1a0b
prodbyjohnwells <153245870+prodbyjohnwells@users.noreply.github.com>
2024-08-11 19:58:12 -0500

    Initialize Repo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <nav>
+        <a href='/'>
+        img src='logo' alt='logo'/>
+        </a>
+        {/* Mobile navbar */}
+
+        {/* Nav items */}
+      </nav>
+    </>
+  );
+}
+
+export default App;
