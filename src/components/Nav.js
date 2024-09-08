01ade14a1db79c721e2103a29270a97e64a4d6e2
prodbyjohnwells <153245870+prodbyjohnwells@users.noreply.github.com>
2024-09-08 16:54:16 -0500

    Updated nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from '../images/Logo .svg'
+import logo from '../images/Logo .svg';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
